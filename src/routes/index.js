a32ece430e1b836c8feaa290d2560c01d50719cf
Carlos Eduardo <72894980+carloseduardob94@users.noreply.github.com>
2022-07-26 13:59:09 -0300

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+const routes = Router()
+
+const usersRouter = require("./user.routes")
+const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
+
+
+routes.use("/users", usersRouter)
+routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
+
+
+module.exports = routes
